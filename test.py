0edd966973f0d0e82c37907a752e802f3416af68
fillipino2 <84112235+fillipino2@users.noreply.github.com>
2025-07-16 10:28:55 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,27 @@
 import subprocess
 import re
 
-def check_selinux_enabled():
+def check_selinux_target_policy():
     try:
+        verify = subprocess.run("grep -i selinuxtype /etc/selinux/config | grep -v '^#'",
+                                shell=True,
+                                capture_output=True,
+                                text=True)
+        if verify.returncode != 0 or not verify.stdout.strip():
+            return "Fail"
+        verification = verify.stdout.lower().strip()
+        final = verification.split("=", 1)[1].strip()
         result = subprocess.run(["sestatus"], capture_output=True, text=True)
         output = result.stdout.lower()
-
         for line in output.splitlines():
-            if line.startswith("selinux status:"):
-                status = line.split(":", 1)[1].strip()
-                if status == "enabled":
-                    return ("Pass", "SELinux is enabled")
+            if line.startswith("Loaded policy name:"):
+                target = line.split(":", 1)[1].strip()
+                if target == "targeted" and final == "targeted":
+                    return "Pass"
                 else:
-                    return ("Fail", f"SELinux is {status}")
-        return ("Fail", "SELinux status line not found in sestatus output")
-
+                    return "Fail"
+        return "Fail"
     except Exception as e:
-        return ("Error", str(e))
-print(check_selinux_enabled())
+        return f"Error: {e}"
+
+print(check_selinux_target_policy())
