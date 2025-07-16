5cb189b938c5c609cd80f33c63bb623ff7d46319
fillipino2 <84112235+fillipino2@users.noreply.github.com>
2025-07-16 15:20:54 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,30 +3,17 @@ import re
 import os
 import glob
 
-def check_postfix():
+def check_gui():
+
     try:
-        command = subprocess.run("yum list installed postfix",
+        command = subprocess.run("rpm -qa | grep xorg | grep server",
                                  shell=True,
                                  capture_output=True,
                                  text=True)
-        if command.returncode != 0:
-            return ("N/A", "Since postifix is not installed this is N/A")
-        
-        config_check = subprocess.run("postconf -n smtpd_client_restrictions",
-                                      shell=True,
-                                      capture_output=True,
-                                      text=True)
-        if config_check.returncode != 0:
-            return "Not Applicable"
-        
-        output = config_check.stdout.strip()
-
-        if "smtpd_client_restrictions" in output and "=" in output:
-            key, val = [x.strip() for x in output.split("=",1)]
-            if key == "smtpd_client_restrictions" and val == "permit_mynetworks, reject":
-                return "Pass"
-            else:
-                return "Fail"
+        if command.returncode == 0:
+            return "Fail, Unless approved previously"
+        else:
+            return "Pass"
     except Exception as e:
-        return f"Error {e}"
-print(check_postfix())
+        return f"Error: {e}"
+print(check_gui())
