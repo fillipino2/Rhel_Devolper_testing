63b1e05c4ac9e95320d9b46665e6ae9d6eaa2de9
fillipino2 <84112235+fillipino2@users.noreply.github.com>
2025-07-16 10:22:54 -0700

    Update test.py

--- test.py
+++ test.py
@@ -5,15 +5,14 @@ def check_selinux_enabled():
     try:
         result = subprocess.run(["sestatus"], capture_output=True,text=True )
         output = result.stdout.lower()
-        return output
-        # for line in output.splitlines():
-        #     if line.startswith("SELinux status:"):
-        #         status = line.split(":", 1)[1].strip()
-        #         if status == "enabled":
-        #             return "Pass"
-        #         else:
-        #             return "Fail"
-        # return "Fail"
+        for line in output.splitlines():
+            if line.startswith("SELinux status:"):
+                status = line.split(":", 1)[1].strip()
+                if status == "enabled":
+                    return "Pass"
+                else:
+                    return "Fail"
+        return "Fail"
     except Exception as e:
         return f"Error: {e}"
- 
+print(check_selinux_enabled())
