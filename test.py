a0575440c3997cc29159647ddefe60115582f4eb
fillipino2 <84112235+fillipino2@users.noreply.github.com>
2025-07-16 10:34:54 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,25 +3,17 @@ import re
 
 def check_selinux_target_policy():
     try:
-        verify = subprocess.run("grep -i selinuxtype /etc/selinux/config | grep -v '^#'",
-                                shell=True,
-                                capture_output=True,
-                                text=True)
-        if verify.returncode != 0 or not verify.stdout.strip():
-            return "Fail"
-        verification = verify.stdout.lower().strip()
-        final = verification.split("=", 1)[1].strip()
-        result = subprocess.run(["sestatus"], capture_output=True, text=True)
-        output = result.stdout.lower()
+        command = subprocess.run(["sestatus"], capture_output=True,text=True)
+        output = command.stdout.lower()
         for line in output.splitlines():
-            if line.startswith("Loaded policy name:"):
-                target = line.split(":", 1)[1].strip()
-                if target == "targeted" and final == "targeted":
-                    return "Pass"
+            if line.startswith("loaded policy name:"):
+                status = line.split(":", 1)[1].strip()
+                if status == "targeted":
+                    return ("Pass", "SELinux is enforcing targeted policies")
                 else:
-                    return "Fail"
-        return "Fail"
+                    return ("Fail", f"SELinux is {status}")
+        return ("Fail", "SELinux loaded policy name was not found")
     except Exception as e:
-        return f"Error: {e}"
+        return ("Error", str{e})
 
 print(check_selinux_target_policy())
