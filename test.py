9e08d8086c169d124c6ca65590172042be68dc86
fillipino2 <84112235+fillipino2@users.noreply.github.com>
2025-07-16 10:44:36 -0700

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ def check_crypto_policies():
         result = subprocess.run("update-crypto-policies --show", shell=True, capture_output=True, text=True)
         output = result.stdout.lower().strip()
         if output == "future":
-            return "Pass"
+            return ("Pass", "Crypro policies are set to use only secure algorithms")
         else:
-            return "Fail"
+            return ("Fail", f"Crypto policies allow unsecure algorithms {output}")
     except Exception as e:
         return f"Error: {e}"
 
