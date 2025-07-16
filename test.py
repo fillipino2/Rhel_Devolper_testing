8308669c90e97f823a8d0da602371bc333be7a89
fillipino2 <84112235+fillipino2@users.noreply.github.com>
2025-07-16 11:06:31 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,18 +2,17 @@ import subprocess
 import re
 import os
 
-def check_bios_UEFI():
+def check_ctl_alt_del():
     try:
-        firmware = subprocess.run("test -d /sys/firmware/efi && echo UEFI || echo BIOS", shell=True, capture_output=True, text=True)
-        if firmware.stdout.strip().lower() == "uefi":
-            result = subprocess.run("grep -iw grub2_password /boot/efi/EFI/redhat/user.cfg", shell=True, capture_output=True, text=True)
-            output = result.stdout.strip().lower()
-            if output.startswith("grub2_password=grub.pbkdf2.sha512"):
-                return ("Pass", f"grub2_password is secure: {output}")
-            else:
-                return ("Fail", f"grub2_password is unsecure: {output}")
-        return ("N/A", "Only Applicable to UEFI systems")
+        command = subprocess.run("sudo systemctl status ctrl-alt-del.target", shell=True, capture_output=True, text=True)
+        result = command.stdout.strip().lower()
+        for line in result.splitlines():
+            if line.startswith("loaded:"):
+                key_value = [s.strip() for s in line.split(":",1)]
+                if len(key_value) == 2 and "masked" in key_value[1].lower():
+                    return "Pass"
+        return "Fail"
     except Exception as e:
         return f"Error: {e}"
 
-print(check_bios_UEFI())
+print(check_ctl_alt_del())
