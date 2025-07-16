347db9da7694898438b05589bdd7f658d62547c9
fillipino2 <84112235+fillipino2@users.noreply.github.com>
2025-07-16 15:19:25 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,14 +3,30 @@ import re
 import os
 import glob
 
-def check_promisc_mode():
+def check_postfix():
     try:
-        command = subprocess.run("sudo ip link | grep -i promisc", shell=True, capture_output=True, text=True)
-        output = command.stdout.strip()
-        if output:
-            return ("Fail", f"promisc mode is enabled and needs to be turned off: {output}")
-        else:
-            return ("Pass", "promisc mode is disabled")
+        command = subprocess.run("yum list installed postfix",
+                                 shell=True,
+                                 capture_output=True,
+                                 text=True)
+        if command.returncode != 0:
+            return ("N/A", "Since postifix is not installed this is N/A")
+        
+        config_check = subprocess.run("postconf -n smtpd_client_restrictions",
+                                      shell=True,
+                                      capture_output=True,
+                                      text=True)
+        if config_check.returncode != 0:
+            return "Not Applicable"
+        
+        output = config_check.stdout.strip()
+
+        if "smtpd_client_restrictions" in output and "=" in output:
+            key, val = [x.strip() for x in output.split("=",1)]
+            if key == "smtpd_client_restrictions" and val == "permit_mynetworks, reject":
+                return "Pass"
+            else:
+                return "Fail"
     except Exception as e:
-        return f"Error: {e}"
-print(check_promisc_mode())
+        return f"Error {e}"
+print(check_postfix())
