00429affe2174a99b4ff0ed92a7b4a295eccd874
fillipino2 <84112235+fillipino2@users.noreply.github.com>
2025-07-16 14:43:46 -0700

    Update test.py

--- test.py
+++ test.py
@@ -7,12 +7,11 @@ def check_audit_services():
         command = subprocess.run("systemctl status auditd.service", shell=True, capture_output=True, text=True)
         output = command.stdout.strip()
         for line in output.splitlines():
-            if line.startswith("Active:"):
+            if line.lstrip().startswith("Active:"):
                     if "active (running)" in line.lower():
                         return "Pass"
                     else:
                         return "Fail"
     except Exception as e:
-        return f"Error: {e}"
 
 print(check_audit_services())
