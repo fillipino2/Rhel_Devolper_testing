c52ede7b65fdd7763b1167118705e9c5c3815767
fillipino2 <84112235+fillipino2@users.noreply.github.com>
2025-07-16 14:57:04 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,14 +2,15 @@ import subprocess
 import re
 import os
 
-def check_ftp_package():
-    try:
-        command = subprocess.run("sudo yum list installed | grep ftpd", shell=True, capture_output=True, text=True)
-        if command.returncode == 0:
-            return ("Fail", f"ftpd package was found: {command.stdout}")
-        else:
-            return ("Pass", "No ftpd package was found was found")
-    except Exception as e:
-        return f"Error: {e}"
+def check_rsh_server_package():
+        try:
+            command = subprocess.run("yum list installed  rsh-server",shell=True,capture_output=True,text=True)
+        
+            if command.returncode == 0:
+                return ("Fail", f"rsh-server has been installed and needs to be removed: {command.stdout}")
+            else:
+                return ("Pass", "rsh-server is not installed")
+        except Exception as e:
+            return f"Error {e}"
 
-print(check_ftp_package())
+print(check_rsh_server_package())
