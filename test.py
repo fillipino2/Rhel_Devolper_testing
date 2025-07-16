d003e0f0e8c2dc2c78fd051916a0970973e52dcc
fillipino2 <84112235+fillipino2@users.noreply.github.com>
2025-07-16 15:02:37 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,43 @@ import subprocess
 import re
 import os
 
-def check_shosts_equiv_file():
+def check_icmp_redirect():
     try:
-        command = subprocess.run("find / -name '*.shosts.equiv' 2>/dev/null", shell=True, capture_output=True, text=True)
+        file_list =["/run/sysctl.d/*.conf",
+                    "/usr/local/lib/sysctl.d/*.conf",
+                    "/usr/lib/sysctl.d/*.conf",
+                    "/lib/sysctl.d/*.conf",
+                    "/etc/sysctl.conf",
+                    "/etc/sysctl.d/*.conf"]
+        
+        command = subprocess.run("sysctl net.ipv4.conf.all.accept_redirects", shell=True,
+                                 capture_output=True,
+                                 text=True)
         output = command.stdout.strip()
-        if output:
-            return ("Fail", f"shosts.equiv file was found: {command.stdout}")
-        else:
-            return ("Pass", "No shosts.equiv file was found")
-    except Exception as e:
-        return f"Error: {e}"
+        if output != "net.ipv4.conf.all.accept_redirects = 0":
+            return "Fail"
+        files_to_check = []
+        for path in file_list:
+            if "*" in path:
+                files_to_check.extend(glob.glob(path))
+            else:
+                files_to_check.append(path)
 
-print(check_shosts_equiv_file())
+        for file_path in files_to_check:
+            try:
+                with open(file_path, "r")as fp:
+                    for line in fp:
+                        line = line.strip()
+                        if not line or line.startswith("#"):
+                            continue
+                        if "net.ipv4.conf.all.accept_redirects" in line and "=" in line:
+                            key, val = [x.strip() for x in line.split("=", 1)]
+                            if key == "net.ipv4.conf.all.accept_redirects" and val != "0":
+                                return "Fail"
+            except Exception:
+                continue
+        return "Pass"
+    except Exception as e:
+            return f"Error: {e}"
+
+print(check_icmp_redirect())
